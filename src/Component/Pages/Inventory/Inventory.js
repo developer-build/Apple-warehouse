25faa1883f281e2507b69e8ad3f81a0814019aa8
developer-sahadat <mdridoysikder862@gmail.com>
2022-04-30 00:55:52 +0600

    inventory page design

--- src/Component/Pages/Inventory/Inventory.js
+++ src/Component/Pages/Inventory/Inventory.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import "./Inventory.css";
 
 const Inventory = () => {
   const { id } = useParams();
 
   return (
     <div>
-      <h1>Hello</h1>
-      <p>{id}</p>
+      <div className="add_quantity-container">
+        <h3>Increase the quantity of the product</h3>
+        <form>
+          <input
+            type="number"
+            placeholder="Enter quantity number"
+            name="quantity"
+          />
+          <button>Submit</button>
+        </form>
+      </div>
     </div>
   );
 };
