e9ad1762a8b98c2afb3c4351050b05017d14b640
developer-sahadat <mdridoysikder862@gmail.com>
2022-04-30 01:57:26 +0600

    add new items added

--- src/Component/Pages/AddNewItem/AddNewItem.js
+++ src/Component/Pages/AddNewItem/AddNewItem.js
@@ -1,9 +1,28 @@
 import React from "react";
+import "./AddNewItem.css";
 
 const AddNewItem = () => {
   return (
-    <div>
-      <h3>Add New Items</h3>
+    <div className="add_new_items_container">
+      <form className="add_new_items_form">
+        <h3>Add New Items</h3>
+        <div>
+          <input type="text" placeholder="Enter title" name="title" />
+          <input type="number" placeholder="Enter Price" name="price" />
+        </div>
+        <div>
+          <input type="text" placeholder="Supplier Name" name="supplierName:" />
+          <input type="number" placeholder="Enter Quantity" name="quantity" />
+        </div>
+        <div className="mb-3">
+          <input type="text" placeholder="Images URl" name="img" />
+          <input type="text" placeholder="Enter Sell" name="sell" />
+        </div>
+        <textarea placeholder="description"></textarea>
+        <div>
+          <button className="Add-items-btn">Add Now</button>
+        </div>
+      </form>
     </div>
   );
 };
